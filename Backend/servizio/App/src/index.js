7f71e6a2c30afb73b60bda0eef01669682117be5
deggio <danideg12@gmail,com>
2025-10-09 04:00:38 +0200

    REFRACTOR: Organized files

---
+++ Backend/servizio/App/src/index.js
@@ -0,0 +1,12 @@
+const fastify = require('fastify')({ logger: true});
+
+//importa serviz
+fastify.register(require('./services/health'));
+
+fastify.listen({ port: 3000, host: '0.0.0.0' }, (err, address) => {
+	if (err) {
+		fastify.log.error(err);
+		process.exit(1);
+	}
+	fastify.log.info('server listening on ${address}');
+});
