2017c06aa6a5e610148509d7574292ab5846662e
alesac1 <alesacchi1907@gmail.com>
2025-11-23 21:07:24 +0000

    login

--- Frontend/src/main.ts
+++ Frontend/src/main.ts
@@ -2,6 +2,8 @@ import './style.css'
 import { Router } from './core/router';
 import { renderHome } from './pages/home';
 import { renderGame } from './pages/game';
+import { renderLogin } from './pages/login';
+import { renderRegister } from './pages/register';
 import { renderTournament } from './pages/tournament';
 import { renderNotFound } from './pages/notFound';
 
@@ -11,6 +13,8 @@ const router = new Router();
 
 // Define routes
 router.addRoute('/', () => renderHome(app, router));
+router.addRoute('/login', () => renderLogin(app, router));
+router.addRoute('/register', () => renderRegister(app, router));
 router.addRoute('/game', () => renderGame(app, router));
 router.addRoute('/tournament', () => renderTournament(app, router));
 router.addRoute('/404', () => renderNotFound(app, router));
